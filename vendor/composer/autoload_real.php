1dd3331f35fc0e3c8cb8f153ab2494ba8276fe91
Daniel Danchev <ddanchev97@gmail.com>
2023-03-21 18:25:51 +0200

    Added google/cloud-translate service to project and added initial set of files.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit17e30a4265d9ae40f1307685025c48e8
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit17e30a4265d9ae40f1307685025c48e8', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit17e30a4265d9ae40f1307685025c48e8', 'loadClassLoader'));
